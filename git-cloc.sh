8ca0855be588056c87fecaf93ea13a5378d5f6fa
Kael <i@kael.me>
2013-02-19 21:03:13 +0800

    add git-cloc.sh; add basic design of parameters

---
+++ git-cloc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# git cloc 
+#   -f, --from          the beginning date, "2012-01-01"
+#   -t, --to            the end date, "2012-02-01"
+#   -u, --user          the author and user, "Kael". default(or "*") to all user. use -u instead of -a to avoid misunderstanding of "all"; 
+#   -b, --branch        default to "master", if "*" will count lines of all branches without duplication
+#   -r, --recursive     recursively search all sub directories of the specified working directory
+#   -c, --cwd           cwd
+
+#overload arguments
