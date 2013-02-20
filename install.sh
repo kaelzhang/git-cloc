f19f6f5f2aa11f08ff7f987b6e1bb70cbcf7b2ca
Kael <i@kael.me>
2013-02-20 21:30:16 +0800

    fix a bug when exec install.sh outside the git-cloc/ directory

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # copy Git to where the rest of the Git scripts are found.
 des="$(git --exec-path)/git-cloc"
 
-cp git-cloc.sh "$des"
+cp ./git-cloc.sh "$des"
 
 # avoid "Permission denied" error
 chmod 755 "$des"
