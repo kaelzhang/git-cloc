fe8fa1119888f36d133aab16f819a117bd9431c4
Kael <i@kael.me>
2013-04-03 13:45:35 +0800

    add install.sh and uninstall.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p $PREFIX/bin
 
 echo "... installing bins to $PREFIX/bin"
 cp -f bin/git-cloc $DESTDIR$PREFIX/bin
-@chmod 755 $PREFIX/bin/git-cloc
+chmod 755 $PREFIX/bin/git-cloc
 
 echo "... installing man pages to $MANPREFIX"
 cp -f man/git-cloc.1 $MANPREFIX
